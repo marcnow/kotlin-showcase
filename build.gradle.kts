b8e554faf346251023f443cf19f6af8b1ac373dd
Marc Nowakowski <marc.nowakowski@hotmail.com>
2022-05-09 13:12:11 +0200

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,13 +27,20 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-    // Kotest
-    testImplementation("io.kotest:kotest-runner-junit5:5.1.0")
-    testImplementation("io.kotest:kotest-assertions-core:5.1.0")
-    testImplementation("io.kotest:kotest-property:5.1.0")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0")
-
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    // Kotest
+    testImplementation("io.kotest:kotest-runner-junit5:5.2.0")
+    testImplementation("io.kotest:kotest-assertions-core:5.2.0")
+    testImplementation("io.kotest:kotest-property:5.2.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.0")
+
+    // Test Container
+    testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:1.3.3")
+
+    // REST Assured
+    testImplementation("io.rest-assured:rest-assured:4.4.0")
 
     // Spring Boot Admin
     implementation("de.codecentric:spring-boot-admin-starter-client:2.6.0")
@@ -74,7 +81,5 @@ tasks.withType<Test> {
 
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
-    reports {
-        xml.isEnabled = true
-    }
+
 }
